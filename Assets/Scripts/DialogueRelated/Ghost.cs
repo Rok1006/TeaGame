20f19b2edd9a71f4b6e8980a420bd19de3ca4760
StevoPineapple <new.logintesting@gmail.com>
2021-12-03 18:43:47 -0500

    Ghost Class stuff(with animator and txt)

---
+++ Assets/Scripts/DialogueRelated/Ghost.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class Ghost : MonoBehaviour
+{
+    #region TextReader
+    TextAsset txtFile;
+
+    [System.Serializable]
+    public class StageData
+    {
+        public List<DialogData> dialogList = new List<DialogData>();
+        public List<DialogData> TeaReactList = new List<DialogData>();
+    }
+    [System.Serializable]
+    public class DialogData
+    {
+        public string dialog;
+        public float pauseTime;
+        public string animType;
+    }
+    #endregion
+
+    public List<StageData> stageList = new List<StageData>();
+    public Text textDisplay;
+    public float typingSpeed = 0.2f;
+    public int stageIndex = 0; //real stage number -1
+    public int dialogIndex = 0; //the line we are currently on
+    Coroutine dialogLoopCor;
+    int wrongCount = 0;
+
+    public Animator anim;
+
+    void Start()
+    {
+        ReadDialogText();
+        ReadReactText();
+        textDisplay.text = "";
+        StartCoroutine(StageLoop());
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DrinkTea("Bad");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DrinkTea("Good");
+        }
+    }
+    IEnumerator StageLoop() //Main loop for each stage
+    {
+        Debug.Log("Intro Animation HERE");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Start dialog");
+        dialogLoopCor = StartCoroutine(TypeAll());
+    }
+    IEnumerator TypeAll() //Go through every dialog of the stage
+    { 
+        for(int i=dialogIndex; i<stageList[stageIndex].dialogList.Count; i++)
+        {
+            DialogData dialogData = stageList[stageIndex].dialogList[i];
+            Animate(dialogData.animType);
+            foreach (char letter in dialogData.dialog.ToCharArray())
+            {
+                textDisplay.text += letter;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+            yield return new WaitForSeconds(dialogData.pauseTime);
+            textDisplay.text = "";
+            dialogIndex++;
+        }
+        Animate(null); //End of all dialog reset animation
+        //Player game over here!!!
+    }
+    IEnumerator Type(DialogData dialogData) //Go through one specific dialog
+    {
+        Debug.Log("Drink Reacting...");
+        Animate(dialogData.animType);
+        foreach (char letter in dialogData.dialog.ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        yield return new WaitForSeconds(dialogData.pauseTime);
+        textDisplay.text = "";
+        dialogLoopCor = StartCoroutine(TypeAll());  //Back to main dialog list
+    }
+    void DrinkTea(string teaType)
+    {
+        Debug.Log("Drink " + teaType);
+        if (teaType == "Bad")
+        {
+            //Stop everything now
+            textDisplay.text = "";
+            StopCoroutine(dialogLoopCor);
+            Animate(null);
+
+            //Start React
+            StartCoroutine(Type(stageList[stageIndex].TeaReactList[wrongCount]));//Get dialog based on wrongCount
+            wrongCount++;
+        }
+        if (teaType == "Good")
+        {
+            //Stop everything now
+            textDisplay.text = "";
+            StopCoroutine(dialogLoopCor);
+            Animate(null);
+
+            //Next Stage
+            wrongCount = 0;
+            stageIndex++;
+            dialogIndex = 0;
+            StartCoroutine(StageLoop());
+        }
+    }
+
+    void Animate(string animType)
+    {
+        if (!string.IsNullOrEmpty(animType))
+        {
+            animType = Regex.Replace(animType, @"\t|\n|\r", "");//Regex to remove the pesky '\r' (<-what a jerk)
+        }
+        switch (animType)
+        {
+            case ("Flip"):
+                {
+                    anim.SetBool("toFlip", true);
+                    break;
+                }
+            case (null): //empty case, set everything to false!
+                {       //It's null so you don't need to write <a> in txt if no animation
+                    anim.SetBool("toFlip", false);
+                    break;
+                }
+        }
+    }
+
+    void ReadDialogText()
+    {
+        //Reading txt
+        txtFile = Resources.Load<TextAsset>("GhostData"); //Read the file
+        List<string> lineTextList = new List<string>(txtFile.text.Split('\n')); //Spilt file to lines
+
+        //Processing txt
+        StageData currStage = new StageData(); // empty new stage and dialog to use
+        DialogData currDialog = new DialogData();
+
+        foreach (string lineText in lineTextList)
+        {
+            if (lineText.Contains("<Stage>"))//Create and store stagedata on <Stage>
+            {
+                currStage = new StageData();
+                stageList.Add(currStage);
+                Debug.Log("ReadingReact" + lineText + "...");
+            }
+            else
+            {
+                if (lineText.Contains("<p>")) //read and store pausetime
+                {
+                    currDialog.pauseTime = float.Parse(lineText.Substring(lineText.IndexOf(">") + 1));
+
+                }
+                else if (lineText.Contains("<a>")) //read and store animation type
+                {
+                    currDialog.animType = lineText.Substring(lineText.IndexOf(">") + 1);
+                }
+                else //read and stor sentence
+                {
+                    currDialog = new DialogData();
+                    currStage.dialogList.Add(currDialog);
+                    currDialog.dialog = lineText;
+                }
+            }
+        }
+        Debug.Log("Reading React done.");
+    }
+    void ReadReactText()
+    {
+        //Reading React txt
+        txtFile = Resources.Load<TextAsset>("TeaReact"); //Read the file
+        List<string> lineTextList = new List<string>(txtFile.text.Split('\n')); //Spilt file to lines
+
+        //Processing txt
+        int stageInd = 0;
+        StageData currStage = new StageData();
+        DialogData currDialog = new DialogData();
+
+        foreach (string lineText in lineTextList)
+        {
+            if (lineText.Contains("<Stage>"))//Create and store stagedata on <Stage>
+            {
+                currStage = stageList[stageInd];
+                Debug.Log("Reading" + lineText + "...");
+            }
+            else
+            {
+                if (lineText.Contains("<p>")) //read and store pausetime
+                {
+                    currDialog.pauseTime = float.Parse(lineText.Substring(lineText.IndexOf(">") + 1));
+
+                }
+                else if (lineText.Contains("<a>")) //read and store animation type
+                {
+                    currDialog.animType = lineText.Substring(lineText.IndexOf(">") + 1);
+                }
+                else //read and stor sentence
+                {
+                    currDialog = new DialogData();
+                    currStage.TeaReactList.Add(currDialog);
+                    currDialog.dialog = lineText;
+                }
+            }
+        }
+        Debug.Log("Reading done.");
+    }
+}
+

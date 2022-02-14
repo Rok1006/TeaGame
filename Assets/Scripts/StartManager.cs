b5ec4d07ad0145ab7c5b778fe004b980a673e191
WenxuanLiao <67645727+WenxuanLiao@users.noreply.github.com>
2022-02-13 21:25:28 -0500

    push

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -10,19 +10,34 @@ public class StartManager : MonoBehaviour
     public GameObject Camera;
     Animator camAnim;
     public GameObject ControlPanel;
+<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
+=======
+    public GameObject Collection_UI;
+    // [Header("LevelSelection")]
+>>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
     void Start()
     {
         camAnim = Camera.GetComponent<Animator>();
         FadeIn.SetActive(false);
         ControlPanel.SetActive(false);
+<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
     }
 
     void Update()
+=======
+        Collection_UI.SetActive(false);
+
+}
+void Update()
+>>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
     {
         
     }
     public void StartButton(){
         StartCoroutine(IntoPlayScene());
+    }
+    public void CollectionButton() {
+        Collection_UI.SetActive(true);
     }
     IEnumerator IntoPlayScene(){
         camAnim.SetTrigger("start");
@@ -33,6 +48,8 @@ public class StartManager : MonoBehaviour
 
 
     }
+
+
     void LoadScene(){
         SceneManager.LoadScene(sceneName);
     }
@@ -42,4 +59,20 @@ public class StartManager : MonoBehaviour
     public void BackButton(){
         ControlPanel.SetActive(false);
     }
+<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
+=======
+
+    public void BackButton_2()
+    {
+        Collection_UI.SetActive(false);
+    }
+
+    //LevelSelection Function
+    public void GetDayNum(int num){ //assign the num back in inspector add to button
+       LevelData.Instance.ghostNum = num;
+    }
+    public void GetCustomerName(string t){ //assign the num back in inspector add to button
+       LevelData.Instance.title = t;
+    }
+>>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
 }

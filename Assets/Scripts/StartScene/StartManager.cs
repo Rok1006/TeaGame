2912b2a9b8e40f0c06f4be0a4a0ff9f4c1956a74
ROK <hilary0310@gmail.com>
2022-02-14 11:33:20 -0500

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 783e2c36eac6d31cca43fc216a999833fa521c79.

--- Assets/Scripts/StartScene/StartManager.cs
+++ Assets/Scripts/StartScene/StartManager.cs
@@ -10,34 +10,19 @@ public class StartManager : MonoBehaviour
     public GameObject Camera;
     Animator camAnim;
     public GameObject ControlPanel;
-<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
-=======
-    public GameObject Collection_UI;
     // [Header("LevelSelection")]
->>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
     void Start()
     {
         camAnim = Camera.GetComponent<Animator>();
         FadeIn.SetActive(false);
         ControlPanel.SetActive(false);
-<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
     }
-
     void Update()
-=======
-        Collection_UI.SetActive(false);
-
-}
-void Update()
->>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
     {
         
     }
     public void StartButton(){
         StartCoroutine(IntoPlayScene());
-    }
-    public void CollectionButton() {
-        Collection_UI.SetActive(true);
     }
     IEnumerator IntoPlayScene(){
         camAnim.SetTrigger("start");
@@ -45,11 +30,7 @@ void Update()
         FadeIn.SetActive(true);
         yield return new WaitForSeconds(1.5f);
         LoadScene();
-
-
     }
-
-
     void LoadScene(){
         SceneManager.LoadScene(sceneName);
     }
@@ -59,14 +40,6 @@ void Update()
     public void BackButton(){
         ControlPanel.SetActive(false);
     }
-<<<<<<< Updated upstream:Assets/Scripts/StartManager.cs
-=======
-
-    public void BackButton_2()
-    {
-        Collection_UI.SetActive(false);
-    }
-
     //LevelSelection Function
     public void GetDayNum(int num){ //assign the num back in inspector add to button
        LevelData.Instance.ghostNum = num;
@@ -74,5 +47,4 @@ void Update()
     public void GetCustomerName(string t){ //assign the num back in inspector add to button
        LevelData.Instance.title = t;
     }
->>>>>>> Stashed changes:Assets/Scripts/StartScene/StartManager.cs
 }

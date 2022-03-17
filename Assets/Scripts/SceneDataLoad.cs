bb8f5437f2d5ed6b84500073341d4ab89b1fcf30
ROK <hilary0310@gmail.com>
2022-03-17 17:07:49 -0400

    minor

--- Assets/Scripts/SceneDataLoad.cs
+++ Assets/Scripts/SceneDataLoad.cs
@@ -9,10 +9,12 @@ public class SceneDataLoad : MonoBehaviour
     public GameObject TitleScreen; //is nt on orginally
     public TextMeshProUGUI titleText;
     void Awake() {
-        titleText.text = LevelData.Instance.title.ToString();//display title
-        GameManager.Instance.ghostIndex = LevelData.Instance.ghostNum; //get the num from levelindex
-        //load everything here
-        print("Day "+LevelData.Instance.ghostNum+" - "+LevelData.Instance.title);
+        if(GameObject.Find("LevelData(DoNtDestroy)") != null){
+            titleText.text = LevelData.Instance.title.ToString();//display title
+            GameManager.Instance.ghostIndex = LevelData.Instance.ghostNum; //get the num from levelindex
+            //load everything here
+            print("Day "+LevelData.Instance.ghostNum+" - "+LevelData.Instance.title);
+        }
         //get data from LevelData and proceed to load character and stuff
         Instance = this;
     }

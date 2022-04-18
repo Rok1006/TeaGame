f001977c4b202d1fa8d2b634499e706732d1809c
hongke <hl3407@nyu.edu>
2022-04-18 13:25:00 -0400

    save system can be turned off now

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -4,51 +4,71 @@ using UnityEngine;
 
 public class SaveSystem : MonoBehaviour
 {
+    public bool enableSave = false;
     private void Start()
     {
-        if (ES3.KeyExists("biggestIndex"))
+        if (enableSave)
         {
-            GameObject.Find("GameManager").GetComponent<GameManager>().ghostIndex = ES3.Load<int>("biggestIndex");
+            if (ES3.KeyExists("biggestIndex"))
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().ghostIndex = ES3.Load<int>("biggestIndex");
+            }
         }
     }
     // Start is called before the first frame update
     public void toSave(int index)
     {
-        ES3.Save("ghostIndex", index);
-        int theIndex = ES3.Load<int>("ghostIndex");
-        Debug.Log(theIndex + " saved");
+        if (enableSave)
+        {
+            ES3.Save("ghostIndex", index);
+            int theIndex = ES3.Load<int>("ghostIndex");
+            Debug.Log(theIndex + " saved");
+        }
     }
 
     public int toLoad()
     {
         int theIndex = 0;
-        if (ES3.KeyExists("ghostIndex"))
+        if (enableSave)
         {
-            theIndex = ES3.Load<int>("ghostIndex");
+            
+            if (ES3.KeyExists("ghostIndex"))
+            {
+                theIndex = ES3.Load<int>("ghostIndex");
+            }
+            
         }
         return theIndex;
     }
 
     public void saveBiggest(int index)
     {
-        int theIndex = 0;
-        if (ES3.KeyExists("ghostIndex"))
+        if (enableSave)
         {
-            theIndex = ES3.Load<int>("ghostIndex");
+            int theIndex = 0;
+            if (ES3.KeyExists("ghostIndex"))
+            {
+                theIndex = ES3.Load<int>("ghostIndex");
+            }
+            if (index > theIndex)
+            {
+                theIndex = index;
+            }
+            ES3.Save("biggestIndex", theIndex);
         }
-        if (index > theIndex)
-        {
-            theIndex = index;
-        }
-        ES3.Save("biggestIndex", theIndex);
     }
 
     public int loadBiggest()
     {
         int theIndex = 0;
-        if (ES3.KeyExists("biggestIndex"))
+        if (enableSave)
         {
-            theIndex = ES3.Load<int>("biggestIndex");
+
+            if (ES3.KeyExists("biggestIndex"))
+            {
+                theIndex = ES3.Load<int>("biggestIndex");
+            }
+            
         }
         return theIndex;
     }
@@ -56,22 +76,28 @@ public class SaveSystem : MonoBehaviour
     public List<string> collectionLoad()
     {
         List<string> collection = new List<string>();
-        if (ES3.KeyExists("collection"))
+        if (enableSave)
         {
-            collection = ES3.Load<List<string>>("collection");
+            if (ES3.KeyExists("collection"))
+            {
+                collection = ES3.Load<List<string>>("collection");
+            }
         }
         return collection;
     }
 
     public void collectionSave(string itemName)
     {
-        List<string> collection = new List<string>();
-        if (ES3.KeyExists("collection"))
+        if (enableSave)
         {
-            collection = ES3.Load<List<string>>("collection");
+            List<string> collection = new List<string>();
+            if (ES3.KeyExists("collection"))
+            {
+                collection = ES3.Load<List<string>>("collection");
+            }
+            collection.Add(itemName);
+            ES3.Save("collection", collection);
         }
-        collection.Add(itemName);
-        ES3.Save("collection", collection);
     }
 
 }

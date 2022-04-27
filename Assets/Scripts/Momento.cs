72944f4a42de6bb8abe21cc84ea327b8903b5aa9
ROK <hilary0310@gmail.com>
2022-04-27 01:31:09 -0400

    Momento

---
+++ Assets/Scripts/Momento.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//attach this script to each momento
+public class Momento : MonoBehaviour
+{
+    public string name;
+    [TextArea(15,20)]
+    public string Description;
+    public Outline OC;
+    public Vector3 rotation;
+    void Start()
+    {
+        OC.enabled = false;
+    }
+
+    void Update()
+    {
+        this.transform.eulerAngles = rotation;
+        //this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,0.013f);
+    }
+    void OnMouseOver() {
+        OC.enabled = true;
+    }
+    void OnMouseDown(){
+        //put into collection in start screen tgt with name and description
+        MomentoDisplay.Instance.momentoAnim.SetTrigger("close");
+        Destroy(this.gameObject);
+    }
+    void OnMouseExit(){
+        OC.enabled = false;
+    }
+}
---
+++ Assets/Scripts/MomentoDisplay.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//assign this script at some manager or sth
+public class MomentoDisplay : MonoBehaviour
+{
+    public static MomentoDisplay Instance;
+    public GameManager GM;
+    public GameObject[] momentoPrefab; //1 = student 
+    int currentMomento;
+    public GameObject currentObj;
+    public GameObject appearPt;
+    public GameObject MomentoBox;
+    public Animator momentoAnim;
+    void Awake() {
+        Instance = this;
+    }
+    void Start()
+    {
+        GM = GameObject.Find("GameManger").GetComponent<GameManager>();
+        momentoAnim = MomentoBox.GetComponent<Animator>();  //give
+    }
+
+    void Update()
+    {
+        switch(GM.ghostIndex){
+            case 0:
+                currentMomento = 0;
+            break;
+            case 1: 
+                currentMomento = 1;
+            break;
+            case 2: 
+                currentMomento = 2;
+            break;
+            case 3: //capitalist
+                currentMomento = 3;
+            break;
+        }
+
+        if(Input.GetKeyDown(KeyCode.P)){ //Testing
+            GiveMomento();  //later add this baak in gamemanager at the right line for each ghost
+        }
+        // if(currentMomento ==null){
+        //     momentoAnim.SetTrigger("Close");
+        // }
+    }
+    public void PlaceInMomento(){
+        GameObject momento = Instantiate(momentoPrefab[currentMomento], appearPt.transform.position, Quaternion.identity) as GameObject;
+        currentObj = momento;
+        //var sc = currentObj.GetComponent<Momento>();
+        currentObj.SetActive(false);
+        currentObj.transform.parent = appearPt.transform;
+        //currentObj.transform.eulerAngles = new Vector3(30, 100, -50);
+    }
+    public void GiveMomento(){
+        StartCoroutine(DisplayMomento());
+    }
+    IEnumerator DisplayMomento(){
+        PlaceInMomento();
+        yield return new WaitForSeconds(1f);
+        momentoAnim.SetTrigger("give");
+        yield return new WaitForSeconds(2f);
+        momentoAnim.SetTrigger("open");
+        yield return new WaitForSeconds(.3f);
+        currentObj.SetActive(true);
+    }
+}
